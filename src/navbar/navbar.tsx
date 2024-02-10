2fe8a451cb5c26e9d0f1c47dff388a5b3505bb41
Kurdanov Feytullah <fkurdan@gmail.com>
2024-02-10 12:02:02 +0300

    Set up a navbar

--- src/navbar/navbar.tsx
+++ src/navbar/navbar.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Logo from './logo'
 import './navbar.css'
+import PreviewButton from './preview-button'
 
 const Navbar = () => {
   return (
     <nav className='navbar'>
         <Logo />
+        <PreviewButton />
     </nav>
   )
 }
---
+++ src/navbar/preview-button.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const PreviewButton = () => {
+    return (
+        <button className='navbar__preview'>
+            <span className='navbar__preview__text'>
+                Preview
+            </span>
+        </button>
+    )
+}
+
+export default PreviewButton
