94d052bcdae044adadc8fa5b29009278d0cd2a20
Kurdanov Feytullah <fkurdan@gmail.com>
2024-02-10 11:10:54 +0300

    Add ts configuration

---
+++ src/navbar/navbar.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Logo from './logo'
+import './navbar.css'
+
+const Navbar = () => {
+  return (
+    <nav className='navbar'>
+        <Logo />
+    </nav>
+  )
+}
+
+export default Navbar
