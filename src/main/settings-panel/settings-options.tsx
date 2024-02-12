f89fc5937fbfe3ca2f59b0e8fd3df68b7b064e19
Kurdanov Feytullah <fkurdan@gmail.com>
2024-02-12 12:51:12 +0300

    Update settings panel input range style

--- src/main/settings-panel/settings-options.tsx
+++ src/main/settings-panel/settings-options.tsx
@@ -1,15 +1,19 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import TranslateXInput from './translate-x-input'
 import TranslateYInput from './translate-y-input'
 
 const SettingsOptions = () => {
+  
+  const TranslateYInputRef = useRef(null)
+
+
   return (
     <ul className='settings-panel__options'>
         <li>
-            <TranslateYInput />
+            <TranslateXInput />
         </li>
         <li>
-            <TranslateXInput />
+            <TranslateYInput />
         </li>
     </ul>
   )
--- src/main/settings-panel/translate-x-input.tsx
+++ src/main/settings-panel/translate-x-input.tsx
@@ -1,16 +1,35 @@
-import React, { useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 
 const TranslateXInput = () => {
     const [inputValue, setInputValue] = useState('0')
+    const translateXInputRef = useRef<HTMLInputElement | null>(null)
 
     const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        translateXInputRef.current = e.target
         setInputValue(e.target.value)
     }
-    
+
+    useEffect(() => {
+        const rangeInput = translateXInputRef.current;
+
+        rangeInput?.addEventListener('input', function () {
+            const value = parseInt(rangeInput.value);
+            const trackWidth = (((Math.abs(value) / 200) * 100) * (0.59)) - 5 + 'px';
+  
+            if (value >= 0) {
+                rangeInput?.style.setProperty('--before-width', '0');
+                rangeInput?.style.setProperty('--after-width', trackWidth);
+            } else {
+                rangeInput?.style.setProperty('--before-width', trackWidth);
+                rangeInput?.style.setProperty('--after-width', '0');
+            }
+        });
+    }, [translateXInputRef])
+
     return (
         <label className='settings-panel__options__option'>
             <span className='settings-panel__options__option__label'>X</span>
-            <input onChange={onChange} type="range" defaultValue={inputValue} min="-200" max="200" />
+            <input ref={translateXInputRef} onChange={onChange} type="range" defaultValue={inputValue} min="-200" max="200" />
             <span className='settings-panel__options__value'>{inputValue}</span>
         </label>
     )
