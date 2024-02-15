bbb56b8133afb36655a9430e221049a1ece2af3a
Kurdanov Feytullah <fkurdan@gmail.com>
2024-02-15 16:16:39 +0300

    Add redux persistor

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,11 +1,23 @@
 import { configureStore } from '@reduxjs/toolkit'
 import animationReducer from './animationSlice'
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+
+const persistConfig = {
+    key: 'root',
+    storage,
+}
+
+const persistedAnimationReducer = persistReducer(persistConfig, animationReducer)
 
 export const store = configureStore({
-    reducer: { 
-        animation: animationReducer 
+    reducer: {
+        animation: persistedAnimationReducer
     },
 })
+
+export const persistor = persistStore(store)
+
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof store.getState>
