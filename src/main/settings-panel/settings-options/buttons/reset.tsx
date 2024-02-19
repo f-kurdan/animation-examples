541f718bba15c0279209471364da7b8e19a733da
Kurdanov Feytullah <fkurdan@gmail.com>
2024-02-19 15:52:14 +0300

    Update run and reset buttons

--- src/main/settings-panel/settings-options/buttons/reset.tsx
+++ src/main/settings-panel/settings-options/buttons/reset.tsx
@@ -4,9 +4,8 @@ import { resetState } from '../../../../redux/animationSlice'
 const Reset = () => {
     const dispatch = useDispatch()
 
-
     return (
-        <div onClick={() => dispatch(resetState())} className='settings-panel__options__run-button'>Reset</div>
+        <div onClick={() => dispatch(resetState())} className='settings-panel__options__button'>Reset</div>
     )
 }
 
--- src/main/settings-panel/settings-options/buttons/run.tsx
+++ src/main/settings-panel/settings-options/buttons/run.tsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import { useDispatch } from 'react-redux'
+import { startAnimation } from '../../../../redux/animationSlice'
 
 const Run = () => {
+    const dispatch = useDispatch()
+
     return (
-        <div className='settings-panel__options__run-button'>Run</div>
+        <div onClick={() => dispatch(startAnimation(true))} className='settings-panel__options__button'>Run</div>
     )
 }
 
