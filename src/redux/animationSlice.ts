00d668ce48fb4e5f1c26fb5792eb4eab31a12226
Kurdanov Feytullah <fkurdan@gmail.com>
2024-02-20 12:59:26 +0300

    Update animation slice

--- src/redux/animationSlice.ts
+++ src/redux/animationSlice.ts
@@ -2,16 +2,17 @@ import { createSlice } from '@reduxjs/toolkit'
 import type { PayloadAction } from '@reduxjs/toolkit'
 
 export interface AnimationState {
-  translateX: number,
-  translateY: number,
-  opacity: number,
-  scale: number,
-  blur: number,
-  speed: number,
-  delay: number,
-  easing: string,
-  replay: boolean,  
-  start: boolean
+    translateX: number,
+    translateY: number,
+    opacity: number,
+    scale: number,
+    blur: number,
+    speed: number,
+    delay: number,
+    easing: string,
+    replay: boolean,
+    start: number
+    repeat: boolean
 }
 
 const initialState: AnimationState = {
@@ -20,51 +21,55 @@ const initialState: AnimationState = {
     opacity: 0,
     scale: 1,
     blur: 0,
-    speed: 0,
+    speed: 1,
     delay: 0,
     easing: 'linear',
-    replay: false, 
-    start: false 
+    replay: false,
+    start: 0,
+    repeat: false
 }
 
 export const animationSlice = createSlice({
-  name: 'counter',
-  initialState,
-  reducers: {
-    setTranslateX: (state, action: PayloadAction<number>) => {
-      state.translateX = action.payload
+    name: 'counter',
+    initialState,
+    reducers: {
+        setTranslateX: (state, action: PayloadAction<number>) => {
+            state.translateX = action.payload
+        },
+        setTranslateY: (state, action: PayloadAction<number>) => {
+            state.translateY = action.payload
+        },
+        setOpacity: (state, action: PayloadAction<number>) => {
+            state.opacity = action.payload
+        },
+        setScale: (state, action: PayloadAction<number>) => {
+            state.scale = action.payload
+        },
+        setBlur: (state, action: PayloadAction<number>) => {
+            state.blur = action.payload
+        },
+        setSpeed: (state, action: PayloadAction<number>) => {
+            state.speed = action.payload
+        },
+        setDelay: (state, action: PayloadAction<number>) => {
+            state.delay = action.payload
+        },
+        setEasing: (state, action: PayloadAction<string>) => {
+            state.easing = action.payload
+        },
+        setReplay: (state, action: PayloadAction<boolean>) => {
+            state.replay = action.payload
+        },
+        resetState: (state) => initialState,
+        increaseAnimationCount: (state) => {
+            state.start += 1
+        },
+        resetAnimationCount: (state) => {
+            state.start = 0
+        },
     },
-    setTranslateY: (state, action: PayloadAction<number>) => {
-        state.translateY = action.payload
-    },
-    setOpacity: (state, action: PayloadAction<number>) => {
-        state.opacity = action.payload
-    },
-    setScale: (state, action: PayloadAction<number>) => {
-        state.scale = action.payload
-    },
-    setBlur: (state, action: PayloadAction<number>) => {
-        state.blur = action.payload
-    },
-    setSpeed: (state, action: PayloadAction<number>) => {
-        state.speed = action.payload
-    },
-    setDelay: (state, action: PayloadAction<number>) => {
-        state.delay = action.payload
-    },
-    setEasing: (state, action: PayloadAction<string>) => {
-        state.easing = action.payload
-    },
-    setReplay: (state, action: PayloadAction<boolean>) => {
-        state.replay = action.payload
-    },
-    resetState: (state) => initialState,
-    startAnimation: (state, action: PayloadAction<boolean>) => {
-        state.start = action.payload
-    }
-  },
 })
 
-export const { setTranslateX, setTranslateY, setOpacity, setScale, setBlur, setSpeed, setDelay, setEasing, setReplay, resetState, startAnimation } = animationSlice.actions
+export const { setTranslateX, setTranslateY, setOpacity, setScale, setBlur, setSpeed, setDelay, setEasing, setReplay, resetState, increaseAnimationCount, resetAnimationCount } = animationSlice.actions
 
 export default animationSlice.reducer
